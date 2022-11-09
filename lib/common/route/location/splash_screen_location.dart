1a37eb36177295794b2d1d0d87d39a2fa2e5ab8d
Jose Santiago Bautista <jose.bautista@whitecloak.com>
2022-11-09 23:04:41 +0800

    fix routing issues

--- lib/common/route/location/splash_screen_location.dart
+++ lib/common/route/location/splash_screen_location.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:website/feature/splash_screen/presentation/screen/screen.dart';
 
 class SplashScreenLocation extends BeamLocation<BeamState> {
-  static const route = 'splash';
+  static const route = '/splash';
 
   @override
   List<BeamPage> buildPages(BuildContext context, BeamState state) {
